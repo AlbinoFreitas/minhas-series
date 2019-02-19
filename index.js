6383b3d7f44cc23a1fe668937807d7aca8166340
AlbinoFreitas <albino_freitas@hotmail.com>
2019-02-19 08:05:19 -0300

    estrutura inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const pages = require('./routes/pages')
+
+const app = express()
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+mongoose.Promise = global.Promise
+
+//process request body
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//assets
+app.use(express.static('public'))
+
+//view engine - ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', pages)
+
+mongoose
+    .connect(mongo, { useNewUrlParser: true })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`server running on port: ${port}`)
+        })
+    })
+    .catch(e => {
+        console.log(e)
+    })
